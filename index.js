43be1949e1a2b1ecae0d9993e83ca574906bd0eb
Isabella <isabella.y.hu@gmail.com>
2022-04-20 20:19:53 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "<h1>Isabella is the champion</h1>";
+
+document.body.appendChild(newHeader);
